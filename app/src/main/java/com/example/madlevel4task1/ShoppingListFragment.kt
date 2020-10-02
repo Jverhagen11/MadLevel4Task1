d97da4e5e6b533b15cc732061afac8e2acfe870f
joel <joel_verhagen_1997@hotmail.com>
2020-10-02 14:17:53 +0200

    productadapter

--- app/src/main/java/com/example/madlevel4task1/ShoppingListFragment.kt
+++ app/src/main/java/com/example/madlevel4task1/ShoppingListFragment.kt
@@ -7,11 +7,25 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.madlevel4task1.Repository.ProductRepository
+import com.example.madlevel4task1.databinding.FragmentShoppinglistBinding
+import kotlinx.android.synthetic.main.fragment_shoppinglist.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class ShoppingListFragment : Fragment() {
+
+    private lateinit var productRepository: ProductRepository
+    private lateinit var binding: FragmentShoppinglistBinding
+    private val mainScope = CoroutineScope(Dispatchers.Main)
+
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -23,7 +37,11 @@ class ShoppingListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        productRepository = ProductRepository(requireContext())
 
     }
+
+
+
+
 }
